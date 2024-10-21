024b6bc980f12ebfc5f19152a04fcdd52cd8e83f
AlexKhabibov <alex.lv.u@gmail.com>
2024-10-21 14:09:08 +0300

    react js sem2HW

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CommentsList from './components/commentsList';
+
+function App() {
+  return (
+    <div className="App">
+      <CommentsList />
+    </div>
+  );
+}
+
+export default App;
